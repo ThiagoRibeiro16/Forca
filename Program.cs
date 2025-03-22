937703d29ce05e33e7493fc6fd43ecffec6f64ab
ThiagoRibeiro16 <thiago.escola321@gmail.com>
2025-03-22 14:20:01 -0300

    finaliza programa

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿string fraseSecreta;
+string letra;
+
+Console.Write("Digite a palavra secreta: ");
+fraseSecreta = Console.ReadLine()!;
+
+Console.Write("Qual é a letra: ");
+letra = Console.ReadLine()!
+    .Trim()
+    .Substring(0, 1)
+    .ToLower();
+
+bool letraExiste = fraseSecreta
+    .ToLower()
+    .Contains(letra);
+
+Console.WriteLine($"\nA letra \"{letra}\" existe na palavra secreta => {letraExiste}");
